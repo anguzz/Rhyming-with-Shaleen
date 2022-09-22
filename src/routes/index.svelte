5b6ea58d58b8021077b1630946d9d2c0220ebfbb
Angel S <santoyox714@gmail.com>
2022-09-22 14:27:19 -0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,6 @@
 <script lang="ts">
   import SEO from "svelte-seo";
   import About from "$lib/pages/Content.svelte";
-  import Home from "$lib/pages/Home.svelte";
   import Footer from "$lib/pages/Footer.svelte";
 
 /* basic colors at  https://tailwindcss.com/docs/customizing-colors   more at  https://hypercolor.dev/   */ 
@@ -14,12 +13,11 @@
 </script>
 
 <SEO
-  title="Fun Finder"
-  description="Go and have some fun!"
+  title="Rhyming with shaleen"
+  description="Shaleen can indeed rhyme"
   canonical="link"
 />
 
-<Home />
 <!--About me/bio-->
 <About/>
 
